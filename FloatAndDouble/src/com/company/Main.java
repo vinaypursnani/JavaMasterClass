e65a6c24c35bd3951d1f0a7c24e17c3a0558ef6e
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2019-07-21 21:40:38 +0530

    Add files via upload

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Convert a given number of pounds to kilograms
+        // Note: 1 pound is equal to 0.45359237 kilograms.
+        double pound = 56d;
+        double kilogram = pound*0.45359237;
+        System.out.println(" 56 pounds is equal to " +kilogram); System.out.println("kgs");
+    }
+}
