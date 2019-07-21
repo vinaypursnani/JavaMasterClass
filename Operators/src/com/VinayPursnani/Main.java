5589f2b93a3f973b5aa5e8e66c3f788bf0f2aced
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2019-07-21 23:07:36 +0530

    Add files via upload

---
+++ Operators/src/com/VinayPursnani/Main.java
@@ -0,0 +1,21 @@
+package com.VinayPursnani;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double myVariable = 20d;
+        double mySecondVariable = 80d;
+        double result = (myVariable + mySecondVariable)*25;
+        double remainder = result%40;
+        System.out.println("Remainder = " +remainder);
+
+        if (remainder<=20)
+            System.out.println("total was over the limit");
+
+
+
+
+
+    }
+}
