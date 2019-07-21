d8ecb35cb5ef189f8648b5f2d4d8b281fbd93424
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2019-07-21 21:42:07 +0530

    Add files via upload

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // using unicode to print registered character symbol
+        char myChar = '\u00AE';
+        System.out.println("Unicode output was " + myChar);
+    }
+}
